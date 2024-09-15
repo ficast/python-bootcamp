01c40241c723297b83a22ee1e8a5d16c4f395e3d
Filipe Oliveira <fc@tsl.io>
2024-09-15 00:36:23 -0300

    edit: aula01

--- aula01/main.py
+++
@@ -1,7 +0,0 @@
-
-name = input("Digite seu nome?\n")
-salary = float(input("Digite seu salário?\n"))
-bonus = float(input("Digite a porcentagem de bônus?\n"))
-kpi = 1000 + salary * bonus
-
-print("Hello, " + name + "! Seu valor bonus foi de: " + str(kpi))
